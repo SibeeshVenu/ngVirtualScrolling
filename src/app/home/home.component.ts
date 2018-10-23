caa48060bb846a27e06b0cda77fdb41dc9483435
Sibeesh Venu <venu@medialesson.de>
2018-10-23 23:32:44 +0200

    Binding data to movie component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { DataSource, CollectionViewer } from '@angular/cdk/collections';
 import { BehaviorSubject, Subscription, Observable } from 'rxjs';
 import { MovieService } from '../movie.service';
+import { Movie } from '../models/movie';
 
 @Component({
   selector: 'app-home',
@@ -16,19 +17,19 @@ export class HomeComponent {
   ds = new MyDataSource(this.movieService);
 }
 
-export class MyDataSource extends DataSource<string | undefined> {
+export class MyDataSource extends DataSource<Movie | undefined> {
   private length = 100000;
   private pageSize = 100;
-  private cachedData = Array.from<string>({ length: this.length });
+  private cachedData = Array.from<Movie>({ length: this.length });
   private fetchedPages = new Set<number>();
-  private dataStream = new BehaviorSubject<(string | undefined)[]>(this.cachedData);
+  private dataStream = new BehaviorSubject<(Movie | undefined)[]>(this.cachedData);
   private subscription = new Subscription();
 
   constructor(private movieService: MovieService) {
     super();
   }
 
-  connect(collectionViewer: CollectionViewer): Observable<(string | undefined)[]> {
+  connect(collectionViewer: CollectionViewer): Observable<(Movie | undefined)[]> {
     this.subscription.add(collectionViewer.viewChange.subscribe(range => {
       const startPage = this.getPageForIndex(range.start);
       const endPage = this.getPageForIndex(range.end - 1);
@@ -53,16 +54,18 @@ export class MyDataSource extends DataSource<string | undefined> {
     }
     this.fetchedPages.add(page);
 
+
     // Use `setTimeout` to simulate fetching data from server.
     setTimeout(() => {
       this.movieService.get('https://api.themoviedb.org/3/movie/top_rated?api_key=c412c072676d278f83c9198a32613b0d&language=en-US&page=1')
         .subscribe((data) => {
-          console.log(data._body);
+          this.dataStream.next(this.cachedData);
+          this.formatDta(JSON.parse(data._body).results);
         });
-      this.cachedData.splice(page * this.pageSize, this.pageSize,
-        ...Array.from({ length: this.pageSize })
-          .map((_, i) => `Item #${page * this.pageSize + i}`));
-      this.dataStream.next(this.cachedData);
+
     }, Math.random() * 1000 + 200);
+  }
+  formatDta(_body: Movie[]): any {
+    this.dataStream.next(_body);
   }
 }
--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -12,6 +12,7 @@ export class MovieComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log(this.movie)
   }
 
 }
