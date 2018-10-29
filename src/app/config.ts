3b6378b4ce15e5b7b75dea55cddfc9a0e0b3cbf6
Sibeesh Venu <venu@medialesson.de>
2018-10-29 19:11:50 +0100

    updateing top rated url

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,7 +2,7 @@ const config = {
     api: {
         baseUrl: 'https://api.themoviedb.org/3/movie/',
         apiKey:'&api_key=c412c072676d278f83c9198a32613b0d',
-        topRated:'top_rated&language=en-US&page=1'
+        topRated:'top_rated?language=en-US&page=1'
     }
 }
 export { config };
